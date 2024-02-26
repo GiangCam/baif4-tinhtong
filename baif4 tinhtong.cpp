7147845516d0524f08b274e735b49fc7385f005f
ADMIN <ADMIN@LAB1-MAY10>
2024-02-26 10:23:41 +0700

    Add project files.

---
+++ baif4 tinhtong.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace sdt;
+int main() {
+	int so1, so2, tong;
+	cout << "CHUONG TRINH TINH TONG\n";
+	cout << "nhap so thu nhat"
+		cin >> so1;
+	cout << "nhap so thu hai"
+		cin >> so2;
+	tong = so1 + so2;
+	cout <
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
